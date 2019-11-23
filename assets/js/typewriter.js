ec627691f2f94eb092d76c20c3420573f5753b38
paranoids <57103009+paranoids-info@users.noreply.github.com>
2019-11-23 11:56:19 +0300

    Update typewriter.js

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -52,6 +52,6 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; width: 5px;}";
     document.body.appendChild(css);
 };
